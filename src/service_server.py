ca1a3f61e78b54e78e008b607d2830d67c4e902f
pystokes <ml4forecast@gmail.com>
2019-04-20 13:17:07 +0900

    Add service

---
+++ src/service_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+from ros_practice.srv import WordCount, WordCountResponse
+
+
+def count_words(request):
+    return WordCountResponse(len(request.words.split()))
+
+
+rospy.init_node('service_server')
+
+service = rospy.Service('word_count', WordCount, count_words)
+
+rospy.spin()
