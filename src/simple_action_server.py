0e39ffcfe933445c38859070770c8ac44a37d241
pystokes <ml4forecast@gmail.com>
2019-04-20 15:42:05 +0900

    Add simple action

---
+++ src/simple_action_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+
+import time
+import actionlib
+#from ros_practice.msg import TimerAction, TimerGoal, TimerResult
+from ros_practice.msg import TimerAction, TimerResult
+
+
+def do_timer(goal):
+    start_time = time.time()
+    time.sleep(goal.time_to_wait.to_sec())
+
+    result = TimerResult()
+    result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
+    result.updates_sent = 0 # Temporarily set zero
+
+    server.set_succeeded(result)
+
+
+rospy.init_node('timer_action_server')
+
+server = actionlib.SimpleActionServer('timer', TimerAction, do_timer, False)
+server.start()
+
+rospy.spin()
