87a4b54effc22623db459f7f6392463681e97c6a
Toshiyuki <toshiyuki.kita@nextremer.com>
2019-04-22 19:10:25 +0900

    Add arm_robot

---
+++ my_command/arm_robot.sh
@@ -0,0 +1,2 @@
+rostopic pub -1 /arm_robot/arm1_arm2_joint_position_controller/command std_msgs/Float64 "data: 1.5"
+rostopic pub -1 /arm_robot/arm2_arm3_joint_position_controller/command std_msgs/Float64 "data: 1.5"
---
+++ my_command/simple_arm_robot.sh
@@ -0,0 +1,1 @@
+rostopic pub -1 /arm_robot/arm1_joint_position_controller/command std_msgs/Float64 "data: 0.5"
